094bdc062ea736dc57d26535849817e6af5c09c1
BallaAlexia <csumpsz@gmail.com>
2024-01-23 17:06:20 +0100

    hei

--- greetings.py
+++ greetings.py
@@ -5,3 +5,21 @@
 # - "That's a nice age!"
 # - "You must be very wise!"
 # - "That doesn't seem to be an integer."
+print("What's your name?")
+name = input()
+print(f"Hello {name}, I am your phone book.")
+print(f"How old are you {name}?")
+while True:
+    try:
+        age = int(input())
+    except ValueError: 
+        print("That doesn't seem to be an integer, mate.")
+        continue
+    else:
+        break
+if age <= 18:
+    print("You are so young! Life is ahead of you!")
+elif age > 18 and age < 40:
+    print("That's a nice age, bro!")
+elif age >= 40:
+    print("You must be very wise, bro!")

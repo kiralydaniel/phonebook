12c4f606fe9c6a32cec96fac20c63cd588d41a15
kiralydaniel <kiralydani93@gmail.com>
2024-01-23 14:43:07 +0100

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,7 @@
+# "What's your name?"
+# "Hello XY, I am your phone book."
+# "How old are you?"
+# - "You are so young! Life is ahead of you!"
+# - "That's a nice age!"
+# - "You must be very wise!"
+# - "That doesn't seem to be an integer."
